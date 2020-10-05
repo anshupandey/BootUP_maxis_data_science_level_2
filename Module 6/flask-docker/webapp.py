b9700af35b9b94c91ccd4ec9b675fe0a4128a7e2
anshupandey <anshuspandey@gmail.com>
2020-10-05 08:44:05 +0530

    updated the code base

--- Module 6/flask-docker/webapp.py
+++ Module 6/flask-docker/webapp.py
@@ -3,13 +3,13 @@ from flask import Flask, request, render_template
 import joblib
 import pandas as pd
 import json
+import numpy as np
 
 #create an app using Flask
 app = Flask(__name__)
 
 #load the pickle files
 model = joblib.load("churn_prediction.pkl")
-preprocessor = joblib.load("preprocessor.pkl")
 
 # use generator to trigger a function whenever a request is received on server
 @app.route('/',methods=["POST","GET"])
@@ -28,20 +28,24 @@ def main2():
 
 
 def preprocess(data_dict):
+    # internation plan = no = 0 and yes = 1
+    # vmail_messages = normal_user = 001, no vm plan = 010, HFu - 100
     df = pd.DataFrame(data_dict,index=[0])
     intcols = ['Number vmail messages', 'Total day minutes','Total eve minutes', 'Total night minutes',
                'Total intl minutes','Customer service calls']
     for k in intcols:df[k] = df[k].apply(int)
+    df['International plan'] = np.where(df['International plan']=="Yes",1,0)
     df['vmail_messages'] = pd.cut(df['Number vmail messages'],bins=[0,1,30,200],
                                   labels=['No VM plan','Normal Users','High Frequency users'],
                                   include_lowest=True)
     df.drop(['Number vmail messages'],axis=1,inplace=True)
-    df = df[['International plan', 'vmail_messages', 'Total day minutes',
+    df['HFU'] = np.where(df['vmail_messages']=="High Frequency users",1,0)
+    df['NU'] = np.where(df['vmail_messages']=="Normal Users",1,0)
+    df['NVP'] = np.where(df['vmail_messages']=="No VM plan",1,0)
+    df = df[['HFU','NVP','NU','International plan', 'Total day minutes',
              'Total eve minutes', 'Total night minutes', 'Total intl minutes',
              'Customer service calls']]
-    # preprocessing data for onehotencoding
-    df = preprocessor.transform(df)
     return df
     
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(debug=False,host="0.0.0.0",port=5000)
